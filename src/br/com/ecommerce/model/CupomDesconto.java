0415f685e667189d506ac0892ff72f412af1085b
retr0-r00t <enzoniches@gmail.com>
2026-08-04 10:50:04 -0300

    update

--- src/br/com/ecommerce/model/CupomDesconto.java
+++
@@ -1,4 +0,0 @@
-package br.com.ecommerce.model;
-
-public class CupomDesconto {
-}
--- src/br/com/ecommerce/service/CarrinhoService.java
+++ src/br/com/ecommerce/service/CarrinhoService.java
@@ -37,4 +37,37 @@ public class CarrinhoService {
         return subtotal;
     }
 
+    public void aplicarCupom(String codigo) {
+        if (cuponsValidos.containsKey(codigo)) {
+
+            this.descontoAtual = cuponsValidos.get(codigo);
+            System.out.println("Cupom  '" + codigo + "' aplicado com sucesso");
+        } else {
+            System.out.println("Cupom invalido");
+        }
+    }
+
+    public Double calcularTotalFinal(){
+        double subtotal = calcularSubtotal();
+        double valorDesconto = subtotal * descontoAtual;
+        return subtotal - valorDesconto;
+    }
+    public void finalizarCompra(EstoqueService estoque) {
+        if (itens.isEmpty()){
+            System.out.println("Seu carrinho está vazio! Adicione produtos antes de finalizar.");
+            return;
+        }
+        System.out.println("Finalizando compra. . .");
+        System.out.println("Total a pagar: R$ " + String.format("%.2f", calcularTotalFinal()));
+
+        for(Map.Entry<Produto, Integer> entry : itens.entrySet()) {
+            Produto produto = entry.getKey();
+            int quantidadeComprada = entry.getValue();
+
+            estoque.darBaixaEstoque(produto.getId(), quantidadeComprada);
+        }
+        itens.clear();
+        this.descontoAtual = 0.0;
+        System.out.println("Compra finalizado com Sucesso!");
+    }
 }
