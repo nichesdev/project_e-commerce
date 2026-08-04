2edcb9fd636150c34353485c5119975a7d6da632
retr0-r00t <enzoniches@gmail.com>
2026-08-04 00:03:39 -0300

    update

--- src/br/com/ecommerce/service/CarrinhoService.java
+++ src/br/com/ecommerce/service/CarrinhoService.java
@@ -1,4 +1,16 @@
 package br.com.ecommerce.service;
 
+import br.com.ecommerce.model.Produto;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class CarrinhoService {
+    private Map<Produto, Integer> itens = new HashMap<>();
+    private Map<Produto, Double> cuponsValidos = new HashMap<>();
+    private double descontoAtual = 0.0;
+
+    public void adicionarItem (Produto produto, int quantidade, EstoqueService estoque) {
+        boolean b = !estoque.temEstoqueDisponivel(produto.getId(), quantidade);
+    }
 }
--- src/br/com/ecommerce/service/EstoqueService.java
+++ src/br/com/ecommerce/service/EstoqueService.java
@@ -1,5 +1,6 @@
 package br.com.ecommerce.service;
 
+import br.com.ecommerce.exception.EstoqueInsuficienteException;
 import br.com.ecommerce.exception.ProdutoNaoEncontradoException;
 import br.com.ecommerce.model.Produto;
 
@@ -16,12 +17,35 @@ public class EstoqueService {
         System.out.println("Produto Cadastrado com sucesso no estoque!");
     }
 
-    public Produto buscarProduto(long id) {
+    public Produto buscarProduto(Long id) {
         Produto p = catalogo.get(id);
 
         if(p == null) {
-            throw new ProdutoNaoEncontradoException("Produto com ID " + id + " nao encontrado!");
+            throw new ProdutoNaoEncontradoException("Produto com ID " + id + " não encontrado!");
         }
         return p;
+    }
+    public boolean temEstoqueDisponivel(Long id, int quantidadeDesejada) {
+        int quantidadeAtual = quantidades.getOrDefault(id, 0);
+        return quantidadeAtual >= quantidadeDesejada;
+    }
+    public void darBaixaEstoque(Long id, int quantidadeComprada) {
+        if (!temEstoqueDisponivel(id, quantidadeComprada)) {
+            throw new EstoqueInsuficienteException("Estoque insuficiente para o produto ID " + id);
+        }
+        int quantidadeAtual = quantidades.get(id);
+
+        quantidades.put(id, quantidadeAtual - quantidadeComprada);
+    }
+    public void listarEstoque() {
+        if (catalogo.isEmpty()) {
+            System.out.println("O Estoque está vazio.");
+            return;
+        }
+        System.out.println("\n--- ESTOQUE ATUAL ---");
+        for (Produto p : catalogo.values()) {
+            int qtd = quantidades.get(p.getId());
+            System.out.println(p + " | Quantidade: " + qtd);
+        }
     }
 }
