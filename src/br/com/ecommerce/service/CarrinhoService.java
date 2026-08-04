cc6c04b5d9c1ab5bde822d4eb0be379c1f2c1414
retr0-r00t <enzoniches@gmail.com>
2026-08-04 09:44:18 -0300

    update

--- src/br/com/ecommerce/service/CarrinhoService.java
+++ src/br/com/ecommerce/service/CarrinhoService.java
@@ -1,5 +1,6 @@
 package br.com.ecommerce.service;
 
+import br.com.ecommerce.exception.EstoqueInsuficienteException;
 import br.com.ecommerce.model.Produto;
 
 import java.util.HashMap;
@@ -7,10 +8,22 @@ import java.util.Map;
 
 public class CarrinhoService {
     private Map<Produto, Integer> itens = new HashMap<>();
-    private Map<Produto, Double> cuponsValidos = new HashMap<>();
+    private Map<String, Double> cuponsValidos = new HashMap<>();
     private double descontoAtual = 0.0;
 
-    public void adicionarItem (Produto produto, int quantidade, EstoqueService estoque) {
-        boolean b = !estoque.temEstoqueDisponivel(produto.getId(), quantidade);
+    public CarrinhoService() {
+        cuponsValidos.put("DEV10", 0.10);
+        cuponsValidos.put("DEV20", 0.20);
     }
+
+    public void adicionarItem (Produto produto, int quantidade, EstoqueService estoque) {
+        if (!estoque.temEstoqueDisponivel(produto.getId(), quantidade)) {
+            throw new EstoqueInsuficienteException("Estoque insuficiente para o produto " + produto.getNome());
+        }
+        int qtdAtualNoCarrinho = itens.getOrDefault(produto,0);
+        itens.put(produto, qtdAtualNoCarrinho + quantidade);
+
+        System.out.println(quantidade + "x " + produto.getNome() + " adicionado ao carrinho com sucesso");
+    }
+
 }
